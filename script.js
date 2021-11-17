d74e6f2e509d17e3252b9ccc3fd57f246e5e9724
Danil-Dovhopolyi <65538352+Danil-Dovhopolyi@users.noreply.github.com>
2021-11-17 12:18:05 +0200

    my layoutCV

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelector(".contact-info_text > h1").onmousemove = () => {
+    document.querySelector(".contact-info_text > h1").style.fontSize = 40 + "px";
+}
+document.querySelector(".contact-info_text > h1").onmouseleave = () => {
+    document.querySelector(".contact-info_text > h1").style.fontSize = 30 + "px";
+}
